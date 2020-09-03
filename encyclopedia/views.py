2e20b1fd6626eca3496d9e2400a7bd24bd7391d9
fshanom <filipeshanom@gmail.com>
2020-09-03 17:45:30 -0300

    Search functionality added. To be fixed: 	-When showing similar results at the search page, the titles are in lower case

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -21,3 +21,24 @@ def entry(request, title):
         'title' : title.capitalize()
     })
 
+def search(request):
+    q = request.GET['q']
+    if util.get_entry(q):
+        #redirect to the existing page calling entry()
+        return redirect("entry", title=q)
+
+    #searching for matching entries titles
+
+    #getting all the entries titles in a list
+    all_entries = util.list_entries()
+    #lowering case of the list and the key to avoid comparision problems
+    all_entries = [item.lower() for item in all_entries]
+    key = q.lower()
+    #making a new list with the matching results
+    match_entries = [i for i in all_entries if key in i] 
+
+    #renders the results page passing the list of matching titles
+    return render(request, "encyclopedia/search.html",{
+        'title' : q,
+        'entries' : match_entries
+    })
